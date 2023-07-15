f4ddb53cf0fc10e6ce98487485f5043ae1f3a8a8
Marc Hernandez <marcsh@gmail.com>
2023-07-15 02:08:25 -0700

    Custom subscriber for the console

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 #![allow(dead_code)]
 
-use tracing::{span, Level, field::{AsField, Visit}};
+use tracing::{span, Level, field::{AsField, Visit}, info};
 
 
 mod com;
@@ -34,29 +34,46 @@ pub fn print( str: String ) {
     let main_span = span!(Level::INFO, "it_works()");
     let _span = main_span.enter();
 
-    println!("Hello, world!");
+    info!("{}", str);
+
+    //println!("Hello, world!");
 }
 
 
 use tracing_subscriber::Layer;
 
 
+#[derive(Default)]
 pub struct PrintVisitor {
+    pub visited:bool
 }
+
 
 impl Visit for PrintVisitor {
     fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
         //println!( "{} = {}", field.name(), value. );
-        let name = field.name();
-        let v = format!("HEYA {name:?}: {value:?}");
-        println!( "{}", v );
+
+        if self.visited {
+            print!(" ");
+        } else {
+            self.visited = true;
+        }
+
+        if field.name() == "message" {
+            let v = format!("[{value:?}]");
+            print!( "{}", v );
+        } else {
+            let v = format!("{}: {value:?}", field.name());
+            print!( "{}", v );
+        };
+        
     }
 }
 
 
 
 pub struct CustomLayer {
-    depth: u32,
+    pub depth: u32,
 }
 
 impl Default for CustomLayer {
@@ -65,18 +82,93 @@ impl Default for CustomLayer {
     }
 }
 
+
+#[derive(Debug)]
+struct SpanDepth(usize);
+
+impl CustomLayer {
+    
+
+    pub fn prefix<S>(
+        ctx: &tracing_subscriber::layer::Context<'_, S>,
+        level: &str,
+        prefix: &str
+    ) -> String
+    where
+    S: tracing::Subscriber,
+    S: for<'lookup> tracing_subscriber::registry::LookupSpan<'lookup>,
+    {
+        let cur_count = if let Some(cur_span_id) = ctx.current_span().id() {
+            let cur = ctx.span( cur_span_id ).unwrap();
+            let cur_ext = cur.extensions();
+            let cur_span_data = cur_ext.get::<SpanDepth>().unwrap();
+            cur_span_data.0
+        } else {
+            0
+        };
+
+        let bars = format!("{:|<1$}", "", (cur_count + 1));
+        let full_prefix = format!("{:12}", prefix);
+
+        format!("{full_prefix}{level}{bars}")
+    }
+
+}
+
 impl<S> Layer<S> for CustomLayer
 where
     S: tracing::Subscriber,
+    S: for<'lookup> tracing_subscriber::registry::LookupSpan<'lookup>,
 {
 
     //*
     fn on_event(
         &self,
         event: &tracing::Event<'_>,
-        _ctx: tracing_subscriber::layer::Context<'_, S>,
+        ctx: tracing_subscriber::layer::Context<'_, S>,
     ) {
-        println!("Got event!");
+        //let prefix = CustomLayer::prefix(event, event.metadata().target().to_string());
+
+        /*
+        let cur_count = if let Some(cur_span_id) = ctx.current_span().id() {
+            let cur = ctx.span( cur_span_id ).unwrap();
+            let cur_ext = cur.extensions();
+            let cur_span_data = cur_ext.get::<SpanDepth>().unwrap();
+            cur_span_data.0
+        } else {
+            0
+        };
+
+        let bars = format!("{:|<1$}", "", (cur_count + 1));
+
+        print!("{:8}>{}", event.metadata().target(), bars );
+
+        let mut visitor = PrintVisitor { visited: false };
+        event.record( &mut visitor );
+
+        println!("");
+        */
+
+        let sym = match *event.metadata().level() {
+            Level::TRACE => ".",
+            Level::DEBUG => ":",
+            Level::INFO  => " ",
+            Level::WARN  => "+",
+            Level::ERROR => "*",
+        };
+
+        let prefix = CustomLayer::prefix(&ctx, sym, event.metadata().target());
+        print!("{}", prefix );
+
+        let mut visitor = PrintVisitor { visited: false };
+        event.record( &mut visitor );
+
+        println!("");
+
+
+        //println!("Got event!");
+
+        /*
         println!("  level={:?}", event.metadata().level());
         println!("  target={:?}", event.metadata().target());
         println!("  name={:?}", event.metadata().name());
@@ -87,37 +179,62 @@ where
         //let mut visitor = PrintVisitor::new( &mut event.fields() );
 
         let mut visitor = PrintVisitor {};
-
         event.record( &mut visitor );
+        // */
 
-        println!("DEBUG");
-        println!( "  Debug={:?}", event.metadata())
+        //println!("DEBUG");
+        //println!( "  Debug={:?}", event )
 
     }
 
     fn on_register_dispatch(&self, collector: &tracing::Dispatch) {
         let _ = collector;
-        println!("on_register_dispatch");
+        //println!("on_register_dispatch");
     }
 
     fn on_layer(&mut self, subscriber: &mut S) {
         let _ = subscriber;
-        println!("on_layer");
+        //println!("on_layer");
     }
 
     fn enabled(&self, metadata: &tracing::Metadata<'_>, ctx: tracing_subscriber::layer::Context<'_, S>) -> bool {
         let _ = (metadata, ctx);
-        println!("enabled");
+        //println!("enabled");
         true
     }
 
     fn on_new_span(&self, attrs: &span::Attributes<'_>, id: &span::Id, ctx: tracing_subscriber::layer::Context<'_, S>) {
-        let _ = (attrs, id, ctx);
-        println!("on_new_span");
+        //let _ = (attrs, id, ctx);
+
+        //println!("on_new_span");
+
+        let cur_count = if let Some(cur_span_id) = ctx.current_span().id() {
+            let cur = ctx.span( cur_span_id ).unwrap();
+            let cur_ext = cur.extensions();
+            let cur_span_data = cur_ext.get::<SpanDepth>().unwrap();
+            cur_span_data.0
+        } else {
+            0
+        };
+
+
+        // Get a reference to the internal span data
+        let span = ctx.span(id).unwrap();
+
+        // Get the special place where tracing stores custom data
+        let mut extensions = span.extensions_mut();
+
+
+        // And store our data
+
+        let storage = SpanDepth { 0: cur_count + 1 };
+
+        extensions.insert::<SpanDepth>(storage);
+
     }
 
     fn max_level_hint(&self) -> Option<tracing::metadata::LevelFilter> {
-        println!("max_level_hint");
+        //println!("max_level_hint");
         None
     }
 
@@ -132,28 +249,45 @@ where
     }
 
     fn event_enabled(&self, _event: &tracing::Event<'_>, _ctx: tracing_subscriber::layer::Context<'_, S>) -> bool {
-        println!("event_enabled");
+        //println!("event_enabled");
         true
     }
 
-    fn on_enter(&self, _id: &span::Id, _ctx: tracing_subscriber::layer::Context<'_, S>) {
-        println!("on_enter");
+    fn on_enter(&self, id: &span::Id, ctx: tracing_subscriber::layer::Context<'_, S>) {
+
+        let prefix = CustomLayer::prefix(&ctx, ">", "" );
+
+        println!("{}-------------", prefix )
+        
+
+        //println!("{}{:|<1$}", "", (cur_count + 1));
 
     }
 
-    fn on_exit(&self, _id: &span::Id, _ctx: tracing_subscriber::layer::Context<'_, S>) {
-        println!("on_exit");
+    fn on_exit(&self, id: &span::Id, ctx: tracing_subscriber::layer::Context<'_, S>) {
+        //println!("on_exit");
+
+        //let cur_span_id = ctx.current_span().id().clone();
+
+        let thing = ctx.lookup_current();
+
+        let prefix = CustomLayer::prefix(&ctx, "<", "");
+
+        if let Some(span) = thing {
+            println!("{}|--{:?}--", prefix, span.metadata() )
+        } else {
+            println!("{}|--{}--", prefix, "NO METADATA" )
+        }
 
     }
 
     fn on_close(&self, _id: span::Id, _ctx: tracing_subscriber::layer::Context<'_, S>) {
-        println!("on_close");
-
+        //println!("on_close");
+        //Dont need anything here
     }
 
     fn on_id_change(&self, _old: &span::Id, _new: &span::Id, _ctx: tracing_subscriber::layer::Context<'_, S>) {
         println!("on_id_change");
-
     }
     // */
 
@@ -190,8 +324,10 @@ mod tests {
 
         let main_span = span!(Level::INFO, "it_works()");
         let _span = main_span.enter();
-    
-        println!("Hello, world!");
+
+
+
+        //println!("Hello, world!");
     }
 }
  
